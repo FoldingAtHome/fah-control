2ae6aa0bd0808012b04092e3b1fc5d1cbccf8eb1
eratosthenes <cdberkstresser@protonmail.com>
2020-04-14 11:46:51 -0500

    Fixing multiple cores and work units

--- fah/ClientConfig.py
+++ fah/ClientConfig.py
@@ -161,7 +161,7 @@ class ClientConfig:
             self.updating = False
 
         # Reload queue list
-        for values in sorted(self.queue):
+        for values in self.queue:
             unit_id = values['unit']
             queue_id = values['id']
             status = values['state'].title()
