f8929b6b78788a38de8986c4efadd55e70c3d234
Federico Fissore <federico@fissore.org>
2020-04-20 17:16:47 +0200

    Removed a warning about not having specified the gtk version

--- fah/util/PasswordValidator.py
+++ fah/util/PasswordValidator.py
@@ -19,9 +19,11 @@
 #                                                                              #
 ################################################################################
 
+import gi
+
+gi.require_version('Gtk', '3.0')
 
 from gi.repository import Gtk
-
 from fah.util.EntryValidator import EntryValidator
 
 
