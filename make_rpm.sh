f47fd20896cd53e5f0747425f15546b7b440a01c
Guy Streeter <guy.streeter@gmail.com>
2020-04-25 14:55:17 -0500

    Use "rpmbuild" subir for building, because "rpm" subdir exists

--- make_rpm.sh
+++ make_rpm.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
-# NOTE the python-rpm-macros and python3-rpm-macros packages must be installed.
+# NOTE the python3-rpm-macros package must be installed.
 
 PACKAGE=fahcontrol
 VERSION=$(rpmspec -q --qf '%{VERSION}' ${PACKAGE}.spec | cut -d' ' -f1)
 RELEASE=$(rpmspec -q --qf '%{RELEASE}' ${PACKAGE}.spec | sed -e 's/\.[^.]*$$//')
 VER_REL=${VERSION}-${RELEASE}
-RPM_TARBALL=rpm/SOURCES/${PACKAGE}-${VER_REL}.tar.gz
+RPM_TARBALL=rpmbuild/SOURCES/${PACKAGE}-${VER_REL}.tar.gz
 
 
-rm -rf rpm/{SOURCES,BUILD,BUILDROOT,SPECS,RPMS,SRPMS}/*
-mkdir -p rpm/SOURCES # {SOURCES,BUILD,RPMS,SRPMS}
+rm -rf rpmbuild/{SOURCES,BUILD,BUILDROOT,SPECS,RPMS,SRPMS}/*
+mkdir -p rpmbuild/SOURCES # {SOURCES,BUILD,RPMS,SRPMS}
 git archive --format=tar --prefix=${PACKAGE}-${VER_REL}/ HEAD | gzip -9 > ${RPM_TARBALL}
-rpmbuild -ba --define "_topdir ${PWD}/rpm" ${PACKAGE}.spec
-# fedpkg --release f31 local
+rpmbuild -ba --define "_topdir ${PWD}/rpmbuild" ${PACKAGE}.spec
