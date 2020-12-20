8ae272a679e3e95699e8e9ba4ded52425afbdb70
Mingye Wang <arthur2e5@aosc.io>
2020-12-20 19:40:06 +0800

    Tell cx_Freeze to bring the dlls
    Still stuck on a from . import _gi
    ModuleNotFoundError: No module named 'gi._error'

--- setup.py
+++ setup.py
@@ -71,6 +71,59 @@ if sys.platform == 'darwin':
 elif sys.platform == 'win32':
     from cx_Freeze import setup, Executable
 
+    # Need to search for include files
+    # from https://github.com/achadwick/hello-cxfreeze-gtk/blob/master/setup.py
+    # FIXME: the same issue may happen with py2app. Check that too.
+    common_include_files = []
+    required_dll_search_paths = os.getenv("PATH", os.defpath).split(os.pathsep)
+    required_dlls = [
+        "libgtk-3-0.dll",
+        "libgdk-3-0.dll",
+        "libepoxy-0.dll",
+        "libgdk_pixbuf-2.0-0.dll",
+        "libpango-1.0-0.dll",
+        "libpangocairo-1.0-0.dll",
+        "libpangoft2-1.0-0.dll",
+        "libpangowin32-1.0-0.dll",
+        "libatk-1.0-0.dll",
+    ]
+    for dll in required_dlls:
+        dll_path = None
+        for p in required_dll_search_paths:
+            p = os.path.join(p, dll)
+            if os.path.isfile(p):
+                dll_path = p
+                break
+        assert dll_path is not None, "Unable to locate {} in {}".format(
+            dll,
+            dll_search_path,
+        )
+        common_include_files.append((dll_path, dll))
+
+    # We need the .typelib files at runtime.
+    # The related .gir files are in $PREFIX/share/gir-1.0/$NS.gir,
+    # but those can be omitted at runtime.
+
+    required_gi_namespaces = [
+        "Gtk-3.0",
+        "Gdk-3.0",
+        "cairo-1.0",
+        "Pango-1.0",
+        "GObject-2.0",
+        "GLib-2.0",
+        "Gio-2.0",
+        "GdkPixbuf-2.0",
+        "GModule-2.0",
+    ]
+
+    for ns in required_gi_namespaces:
+        subpath = "lib/girepository-1.0/{}.typelib".format(ns)
+        fullpath = os.path.join(sys.prefix, subpath)
+        assert os.path.isfile(fullpath), "Required file {} is missing".format(
+            fullpath,
+        )
+        common_include_files.append((fullpath, subpath))
+
     # Change base to 'Console' for debugging
     e = Executable(app, base='Win32GUI', icon='images/FAHControl.ico')
     options = {
